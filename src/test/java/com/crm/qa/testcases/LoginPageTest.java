4e290e72ea3da7cd8326899c3f1c31e702e03260
Tushar Goyal <tshrgoyal20@gmail.com>
2022-11-30 13:09:43 +0530

    remove dep.

--- src/test/java/com/crm/qa/testcases/LoginPageTest.java
+++ src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -1,6 +1,5 @@
 package com.crm.qa.testcases;
 
-import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -15,8 +14,6 @@ public class LoginPageTest extends TestBase {
 	LoginPage loginPage;
 	TestUtil testutil;
 	HomePage homepage;
-	
-	Logger log = Logger.getLogger(LoginPageTest.class);
 	
 	public LoginPageTest() {
 		super();
@@ -40,23 +37,19 @@ public class LoginPageTest extends TestBase {
 	@Test(priority=1)
 	public void loginPageTitleTest()
 	{
-		log.info("******** Test Case loginPageTitleTest started ********");
 		String title = loginPage.verifyLoginPageTitle();
 		Assert.assertEquals(title, "Free CRM #1 cloud software for any business large or small");
 		System.out.println("Login Page title is: "+title);
-		log.info("******** Test Case loginPageTitleTest end ********");
 	}
 	
 	@Test(priority=2)
 	public void signInTest()
 	{
-		log.info("******** Test Case signInTest started ********");
 		loginPage.clickOnLogin();
 		//TestUtil.ImplicitWait();
 		loginPage.SignIn(prop.getProperty("username"), prop.getProperty("password"));
 		Assert.assertTrue(homepage.verifyCorrectUsername());
 		System.out.println("User logged in successfully");
-		log.info("******** Test Case signInTest end ********");
 	}
 
 }
